6c7bb25348eed8921048285b4453a8a5b34f3877
Amarildo <amarildos@outloo.it>
2024-01-25 20:49:18 +0100

    added dynamism to clicked contact, and add cont-li

--- js/script.js
+++ js/script.js
@@ -8,7 +8,29 @@ const app = createApp({
     return {
       contacts: data.contacts,
       user: data.user,
+      selectedContact: null,
+      currentDateTime: new Date(),
     };
+  },
+  methods: {
+    // Metodo per impostare il contatto selezionato
+    selectContact(contact) {
+      this.selectedContact = contact;
+    },
+  },
+  formatDateTime(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: false,
+      timeZoneName: "short",
+    };
+    return date.toLocaleString(undefined, options);
   },
 });
 
