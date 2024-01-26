6ef6399b4d13a0a04ed4e55cf2eb50929cd537ff
Amarildo <amarildos@outloo.it>
2024-01-26 15:24:00 +0100

    added function sent message & receive ok message

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ const app = createApp({
     return {
       contacts: data.contacts,
       user: data.user,
+      //Conversazione contatto
       activeContact: null,
+
+      // testo del nuovo messaggio
+      newMessageText: "",
     };
   },
   methods: {
@@ -16,6 +20,36 @@ const app = createApp({
     showConversation(contact) {
       console.log("conversazione di", contact.name);
       this.activeContact = contact;
+    },
+    // Aggiunge un nuovo messaggio e ottiene una risposta dopo 1 secondo
+    addMessage() {
+      if (!this.activeContact || !this.newMessageText) {
+        return;
+      }
+
+      // Aggiungi il messaggio nella conversazione
+      const newMessage = {
+        id: Date.now(),
+        text: this.newMessageText,
+        date: new Date().toISOString(),
+        status: "sent",
+      };
+
+      this.activeContact.messages.push(newMessage);
+
+      // Pulisci il campo di input
+      this.newMessageText = "";
+
+      // risposta ok dopo 1 secondo
+      setTimeout(() => {
+        const replyMessage = {
+          id: Date.now() + 1,
+          text: "Ok",
+          date: new Date().toISOString(),
+          status: "received",
+        };
+        this.activeContact.messages.push(replyMessage);
+      }, 1000);
     },
   },
 });
